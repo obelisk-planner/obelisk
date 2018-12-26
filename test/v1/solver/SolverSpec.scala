159aec124f8595b8efd1ca6434d2ccec43663810
turar <turar@yahoo.com>
2018-12-26 09:09:22 -0500

    Fixed additional compile errors

--- test/v1/solver/SolverSpec.scala
+++ test/v1/solver/SolverSpec.scala
@@ -12,12 +12,6 @@ class SolverSpec extends FlatSpec {
     val potTimeResource = Resource(id = 3, name = "Pot Time", measurementUnit = "Pot Month", naturalProduction = 1)
     val flowerResource = Resource(id = 4, name = "Flower", measurementUnit = "Item", naturalProduction = 0)
 
-    // Resources
-    val waterResource = Resource(id = 1, name = "Water", measurementUnit = "Cup", naturalProduction = 1)
-    val iceResource = Resource(id = 2, name = "Ice", measurementUnit = "Cube", naturalProduction = 0)
-    val potTimeResource = Resource(id = 3, name = "Pot Time", measurementUnit = "Pot Month", naturalProduction = 1)
-    val flowerResource = Resource(id = 4, name = "Flower", measurementUnit = "Item", naturalProduction = 0)
-
     // Recipes
     val freezingRecipe = Recipe(
       id = 1,
@@ -104,7 +98,7 @@ class SolverSpec extends FlatSpec {
 
     // Test
     assert(result.objectiveValue == -1.6666667)
-    assert(result.recipeSolutions(0).solution == 0.33333333333333337))
+    assert(result.recipeSolutions(0).solution == 0.33333333333333337)
     assert(result.recipeSolutions(1).solution == 1.0)
     assert(result.recipeSolutions(1).solution == 0.3333333333333333)
     assert(result.recipeSolutions(1).solution == 0.3333333333333333)
